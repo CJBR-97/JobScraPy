de23f6cb0e805fc747fcd1d49bbda4877b173efb
C. Reimer <69945487+CJBR-97@users.noreply.github.com>
2024-08-18 16:56:17 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     url='https://github.com/CJBR-97/JobScraPy',
     author='Christoff Reimer',
     packages=find_packages(),
-    install_requires=['re', 'time', 'warnings', 'functools', 'traceback', 'pandas', 'bs4', 'selenium'],
+    install_requires=['re', 'datetime', 'time', 'warnings', 'functools', 'traceback', 'pandas', 'bs4', 'selenium'],
     version='0.1',
     license='MIT',
     description='A Python package for materials science from pre-existing code',
