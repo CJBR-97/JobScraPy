d905356ba34773ea7d76a92d93faa9d40c8cae1a
C. Reimer <69945487+CJBR-97@users.noreply.github.com>
2024-08-18 16:46:49 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Job Scraping Tools for Python',
+    url='https://github.com/CJBR-97/JobScraPy',
+    author='Christoff Reimer',
+    packages=find_packages(),
+    install_requires=['re', 'time', 'warnings', 'functools', 'traceback', 'pandas', 'bs4', 'selenium'],
+    version='0.1',
+    license='MIT',
+    description='A Python package for materials science from pre-existing code',
+    long_description=open('README.txt').read(),
+)
